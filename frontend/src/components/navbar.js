0360475ec4c268889d13024816b7072c4725bef7
SubRayZero <jasmine.draperi.simplon@gmail.com>
2024-05-30 14:48:51 +0200

    start front

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+export default function Navbars() {
+    return (
+        <Navbar bg="light" data-bs-theme="light">
+            <Container>
+                <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                    <Nav.Link as={Link} to="/singup">SingUp</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
